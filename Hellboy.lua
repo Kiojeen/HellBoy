9f9f07f2c16320918e55d2de2131065f5df96682
Kiojeen <88953619+Kiojeen@users.noreply.github.com>
2022-09-01 11:50:56 +0300

    Update Hellboy.lua

--- Hellboy.lua
+++ Hellboy.lua
@@ -21,7 +21,7 @@ hellboy = 8065
 sky = {
     live = {
         package = 'com.tgc.sky.android',
-        version = 199070
+        version = 199846
     },
     huawei = {
         package = 'com.tgc.sky.android.huawei',
